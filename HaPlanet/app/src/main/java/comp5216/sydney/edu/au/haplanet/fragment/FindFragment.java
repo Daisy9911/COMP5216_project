0e7ed00bc5784430c05d380ea825c5cc3f9d6e8b
GOLDENFLEECE-PC\user <s.wang.55@student.scu.edu.au>
2022-10-09 13:00:01 +1100

    Find page.

--- HaPlanet/app/src/main/java/comp5216/sydney/edu/au/haplanet/fragment/FindFragment.java
+++ HaPlanet/app/src/main/java/comp5216/sydney/edu/au/haplanet/fragment/FindFragment.java
@@ -2,20 +2,35 @@ package comp5216.sydney.edu.au.haplanet.fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+
+import comp5216.sydney.edu.au.haplanet.AutoFitGridLayoutManager;
+import comp5216.sydney.edu.au.haplanet.model.EventModel;
 import comp5216.sydney.edu.au.haplanet.R;
+import comp5216.sydney.edu.au.haplanet.RecyclerViewAdapter;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link FindFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class FindFragment extends Fragment {
+public class FindFragment extends Fragment implements RecyclerViewAdapter.ItemListener {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -25,6 +40,10 @@ public class FindFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    FirebaseFirestore mFirestore;
+
+    RecyclerView recyclerView;
+    ArrayList<EventModel> arrayList;
 
     public FindFragment() {
         // Required empty public constructor
@@ -51,6 +70,7 @@ public class FindFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mFirestore = FirebaseFirestore.getInstance();
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
@@ -62,5 +82,45 @@ public class FindFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_find, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        recyclerView = getView().findViewById(R.id.recyclerview);
+        arrayList = new ArrayList<>();
+        CollectionReference eventsRef = mFirestore.collection("events");
+        eventsRef.get().addOnCompleteListener((OnCompleteListener<QuerySnapshot>) task -> {
+            if (!task.isSuccessful()) {
+                Log.e("firebase", "Error getting data", task.getException());
+            }
+            else {
+                task.getResult().getDocuments().stream().forEach(e -> {
+                    System.out.println(e);
+                    EventModel eventModel = new EventModel();
+                    eventModel.setDescription(e.getString("description"));
+                    eventModel.setLocation(e.getString("location"));
+                    eventModel.setNumberOfPeople(Math.toIntExact(e.getLong("number_of_people")));
+                    eventModel.setPicture(e.getString("picture"));
+                    eventModel.setPrice(e.getDouble("price"));
+                    eventModel.setStartTime(e.getDate("start_time"));
+                    eventModel.setTitle(e.getString("title"));
+                    arrayList.add(eventModel);
+                });
+
+                RecyclerViewAdapter adapter = new RecyclerViewAdapter(getContext(), arrayList, this);
+                recyclerView.setAdapter(adapter);
+
+                AutoFitGridLayoutManager layoutManager = new AutoFitGridLayoutManager(getContext(), 500);
+                recyclerView.setLayoutManager(layoutManager);
+            }
+        });
+
+
+    }
+
+    @Override
+    public void onItemClick(EventModel item) {
+        Toast.makeText(getContext(), item.getTitle() + " is clicked", Toast.LENGTH_SHORT).show();
     }
 }
---
+++ HaPlanet/app/src/main/java/comp5216/sydney/edu/au/haplanet/model/EventModel.java
@@ -0,0 +1,74 @@
+package comp5216.sydney.edu.au.haplanet.model;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+/**
+ * Created by anupamchugh on 11/02/17.
+ */
+
+public class EventModel {
+    private String description;
+    private String location;
+    private int numberOfPeople;
+    private String picture;
+    private double price;
+    private Date startTime;
+    private String title;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getNumberOfPeople() {
+        return numberOfPeople;
+    }
+
+    public void setNumberOfPeople(int numberOfPeople) {
+        this.numberOfPeople = numberOfPeople;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Date getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Date startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
